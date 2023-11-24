ee7bd47150da3f000d4a5eabfab513e6d35440ae
asheroh <wnstj701@naver.com>
2023-11-24 16:57:17 +0900

    Test: Testing masonry layout

--- src/components/management/Styled.tsx
+++ src/components/management/Styled.tsx
@@ -1,8 +1,54 @@
 import styled from "styled-components";
 
 export const MasonryContainer = styled.div`
+  width: 100%;
+  height: calc(100% - 150px);
+  border: 1px solid #fff;
+`;
+
+export const CardContainer = styled.div`
+  width: 200px;
+  height: auto;
+  border: 1px solid #fff;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  position: relative;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const CardImageSection = styled.section`
+  width: 100%;
+  height: calc(100% - 35px);
+  border: 1px solid #fff;
+`;
+
+export const CardImageStyle = {
+  width: "100%",
+  objectFit: "contain",
+};
+
+export const CardFooter = styled.section`
+  width: 100%;
+  height: 35px;
+  border: 1px solid #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  transition: all 0.3s ease-in-out;
+`;
+
+export const CardButton = styled.section`
+  width: 50%;
+  height: 100%;
+  border: 1px solid #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.3s ease-in-out;
 `;
