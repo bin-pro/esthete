d4824286700f8082d1bab37558a5d5983c125585
Hankyul Min <denirokr22@gmail.com>
2023-12-01 13:08:10 +0900

    Chore: Init admin-service

---
+++ src/test/java/com/example/estheteadminservice/EstheteAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estheteadminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstheteAdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
