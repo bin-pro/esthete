de8622fa63f55056230e77e34b3f90261b8e4fd4
asheroh <wnstj701@naver.com>
2023-12-04 16:44:59 +0900

    Fix: Exchange not-found comp to page

--- src/app/admin/[id]/page.tsx
+++ src/app/admin/[id]/page.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import * as S from "@/components/statistic/Styled";
 import Image from "next/image";
 import Background from "@/../public/images/background.jpg";
 import Header from "@/components/statistic/Header";
 
-const Admin: React.FC = () => {
+const Admin = () => {
   return (
     <>
       <S.Container>
