e5070fc417233343f17b2435c761a820768c3e52
asheroh <wnstj701@naver.com>
2023-12-04 15:36:49 +0900

    Fix: Add React.FC on admin page

--- src/app/admin/[id]/page.tsx
+++ src/app/admin/[id]/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Background from "@/../public/images/background.jpg";
 import Header from "@/components/statistic/Header";
 
-const Admin = () => {
+const Admin: React.FC = () => {
   return (
     <>
       <S.Container>
