151c28912a8806357bd3d345613216caf19db938
Hankyul Min <denirokr22@gmail.com>
2023-12-06 21:29:57 +0900

    Fix: Photo, GuestBook delete logic

--- src/main/java/com/example/estheteadminservice/service/GuestBookServiceImpl.java
+++ src/main/java/com/example/estheteadminservice/service/GuestBookServiceImpl.java
@@ -1,13 +1,16 @@
 package com.example.estheteadminservice.service;
 
 import com.example.estheteadminservice.dto.GuestBookDto;
+import com.example.estheteadminservice.entity.GuestBook;
 import com.example.estheteadminservice.exception.GuestBookErrorResult;
 import com.example.estheteadminservice.exception.GuestBookException;
+import com.example.estheteadminservice.repository.GuestBookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
@@ -17,11 +20,19 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class GuestBookServiceImpl implements GuestBookService {
 
+    private final GuestBookRepository guestBookRepository;
+
     @Value("${services.core-service}")
     private String CORE_SERVICE;
 
     @Override
+    @Transactional
     public GuestBookDto.DeleteResponse deleteGuestBook(UUID guestBookId) {
+
+        final GuestBook guestBook = guestBookRepository.findByGuestBookId(guestBookId)
+                .orElseThrow(() -> new GuestBookException(GuestBookErrorResult.GUEST_BOOK_NOT_FOUND));
+
+        guestBookRepository.delete(guestBook);
 
         WebClient webClient = WebClient.builder()
                 .baseUrl(CORE_SERVICE)
--- src/main/java/com/example/estheteadminservice/service/PhotoServiceImpl.java
+++ src/main/java/com/example/estheteadminservice/service/PhotoServiceImpl.java
@@ -2,15 +2,18 @@ package com.example.estheteadminservice.service;
 
 import com.example.estheteadminservice.dto.GuestBookDto;
 import com.example.estheteadminservice.dto.PhotoDto;
+import com.example.estheteadminservice.entity.Photo;
 import com.example.estheteadminservice.exception.GuestBookErrorResult;
 import com.example.estheteadminservice.exception.GuestBookException;
 import com.example.estheteadminservice.exception.PhotoErrorResult;
 import com.example.estheteadminservice.exception.PhotoException;
+import com.example.estheteadminservice.repository.PhotoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
@@ -20,11 +23,20 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class PhotoServiceImpl implements PhotoService {
 
+    private final PhotoRepository photoRepository;
+
     @Value("${services.core-service}")
     private String CORE_SERVICE;
 
     @Override
+    @Transactional
     public PhotoDto.DeleteResponse deletePhoto(UUID photoId) {
+
+        final Photo photo = photoRepository.findByPhotoId(photoId)
+                .orElseThrow(() -> new PhotoException(PhotoErrorResult.PHOTO_NOT_FOUND));
+
+        photoRepository.delete(photo);
+
         WebClient webClient = WebClient.builder()
                 .baseUrl(CORE_SERVICE)
                 .build();
